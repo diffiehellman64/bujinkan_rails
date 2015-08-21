32ce67d00bbde91eef1dbe37a9fabac444ebec7e
diffiehellman <diffiehellman@mail.ru>
2015-08-21 23:39:49 +0300

    mailer

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,4 +3,9 @@ class MainController < ApplicationController
   def index
   end
 
+  def send_mail
+    ApplicationMailer.send_mail('ok!').deliver_now
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'main#index'
-
+  get 'send_mail' => 'main#send_mail', as: 'send_mail'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
